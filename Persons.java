92096ab4e1c6ed409fb59acb24807755d793d610
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-03-07 19:36:02 +0200

    Add builder pattern for persons

--- Persons.java
+++ Persons.java
@@ -4,17 +4,37 @@ import java.util.Scanner;
 
 // Read persons, read a name to replace, print in tabular form, print average salary.
 
-public class Persons {
-    record Person(String name, int birthYear, int salary) {
-        final static int currentYear = Year.now().getValue();
-        public int age() {
-            return currentYear - birthYear;
-        }
-        @Override
-        public String toString() {
-            return name + " | " + age() + " | " + salary;
-        }
+record Person(String name, int birthYear, int salary) {
+    final static int currentYear = Year.now().getValue();
+    public int age() {
+        return currentYear - birthYear;
     }
+    @Override
+    public String toString() {
+        return name + " | " + age() + " | " + salary;
+    }
+}
+final class PersonBuilder {
+    private String name;
+    private Integer birthYear;
+    private Integer salary;
+    PersonBuilder name(String name_) {
+        name = name_;
+        return this;
+    }
+    PersonBuilder birthYear(int birthYear_) {
+        birthYear = birthYear_;
+        return this;
+    }
+    PersonBuilder salary(int salary_) {
+        salary = salary_;
+        return this;
+    }
+    Person build() {
+        return new Person(name, birthYear, salary);
+    }
+}
+public class Persons {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         var persons = new ArrayList<Person>();
@@ -25,21 +45,22 @@ public class Persons {
                 break;
             }
             String[] members = data.split(" *, *");
-            persons.add(new Person(members[0], Integer.parseInt(members[1]), Integer.parseInt(members[2])));
+            Person p = new PersonBuilder().name(members[0]).birthYear(Integer.parseInt(members[1])).salary(Integer.parseInt(members[2])).build();
+            persons.add(p);
         }
         System.out.println("Enter a name to replace, and the replacement, separated by commas.");
         String[] data = in.nextLine().split(" *, *");
         for(int x = 0;x < persons.size();x++) {
             Person p = persons.get(x);
-            if(p.name.equals(data[0])) {
-                persons.set(x, new Person(data[1], p.birthYear, p.salary));
+            if(p.name().equals(data[0])) {
+                persons.set(x, new PersonBuilder().name(data[1]).birthYear(p.birthYear()).salary(p.salary()).build());
             }
         }
         double avgSalary = 0;
         System.out.println("NAME | AGE | SALARY");
         for(Person p : persons) {
             System.out.println(p);
-            avgSalary += p.salary;
+            avgSalary += p.salary();
         }
         avgSalary /= persons.size();
         System.out.println("Average salary: " + avgSalary);
