796452a7ecc812537ab3ae3084b659838780708e
Theodor Negrescu <theodor.negrescu@gmail.com>
2024-03-07 18:25:52 +0200

    Add arithmetic mean

---
+++ ArithmeticMean.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class ArithmeticMean {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double a = in.nextDouble();
+        double b = in.nextDouble();
+        System.out.println((a + b) / 2);
+    }
+}
